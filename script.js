c80e918a778613ec0fa2eea9a6c2ee2ae15804ef
Fabian Ternis <151365993+Michaelninder@users.noreply.github.com>
2025-07-25 15:18:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+const displayContent = document.getElementById('display-content');
 const resetBtn = document.getElementById('reset-btn');
-const numberBtns =  document.getElementsByClassName('number');
-const operatorBtns = document.getElementsByClassName('operator');
+const numberBtns = document.querySelectorAll('.input-btn.number .btn-link');
+const operatorBtns = document.querySelectorAll(
+  '.input-btn.operator .operator-btn'
+);
+
+document.addEventListener('DOMContentLoaded', () => {
+  const allButtons = document.querySelectorAll('.input-btn');
+  allButtons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+      e.preventDefault();
+    });
+  });
+});
