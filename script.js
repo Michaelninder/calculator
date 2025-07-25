e820637e8a34813f7526c28e5f4a939a10f4ad1f
Fabian Ternis <151365993+Michaelninder@users.noreply.github.com>
2025-07-25 15:19:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 const displayContent = document.getElementById('display-content');
 const resetBtn = document.getElementById('reset-btn');
 const numberBtns = document.querySelectorAll('.input-btn.number .btn-link');
-const operatorBtns = document.querySelectorAll(
-  '.input-btn.operator .operator-btn'
-);
+const operatorBtns = document.querySelectorAll('.input-btn.operator .operator-btn');
 
 document.addEventListener('DOMContentLoaded', () => {
   const allButtons = document.querySelectorAll('.input-btn');
